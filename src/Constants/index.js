e73621852e22200cdd2d6d38e441b71ed488c173
Chhatish kumar <127961547+ChhatishK@users.noreply.github.com>
2024-10-10 19:43:45 +0530

    Add files via upload

---
+++ src/Constants/index.js
@@ -0,0 +1,93 @@
+import img1 from '../Alabay Assets/DALL·E 2024-07-29 17.36.01 - A 3D cartoon illustration of a cute furry Central Asian Shepherd dog holding a mobile phone and looking at it. The dog has expressive eyes, a fluffy c 2.png'
+import img2 from '../Alabay Assets/DALL·E 2024-07-23 00.50.44 - A 3D cartoon illustration of a furry Central Asian Shepherd Alabay dog with big eyes, wearing a cap with a dollar logo on its head and counting money_ 1.png'
+import img3 from '../Alabay Assets/DALL·E 2024-07-15 01.33.29 - A full-body 3D cute cartoon illustration of a very happy furry Central Asian Shepherd dog dressed in a Viking outfit. The dog should have a big smile_ 1.png'
+import img4 from '../Alabay Assets/DALL·E 2024-07-15 01.32.53 - A full-body 3D cute cartoon illustration of a very happy furry Central Asian Shepherd dog celebrating with a trophy. The dog should have a big smile a 1.png'
+import img5 from '../Alabay Assets/DALL·E 2024-07-15 01.31.03 - A full-body 3D cute cartoon illustration of a very happy furry Central Asian Shepherd dog dressed in a t-shirt with an Alabay dog printed on it and we 2.png'
+import img6 from '../Alabay Assets/DALL·E 2024-07-15 01.28.43 - A full body 3D cute cartoon illustration of a very happy furry Central Asian Shepherd dog dressed as a construction worker. The dog should have a big_ 3.png'
+import img7 from '../Alabay Assets/DALL·E 2024-07-14 21.37.17 - A 3D cartoon illustration of a very happy furry Central Asian Shepherd dog in explorer attire, standing at a crossroads and holding a map with a yello 2.png'
+
+import ROAD_MAP from '../Alabay Assets/DALL·E 2024-07-14 21.37.17 - A 3D cartoon illustration of a very happy furry Central Asian Shepherd dog in explorer attire, standing at a crossroads and holding a map with a yello 2.png'
+
+import blackBG from '../Alabay Assets/Alabay Merch/black hoodie bck.png'
+import blackHoodies from '../Alabay Assets/Alabay Merch/black hoodie.png'
+import blueBG from '../Alabay Assets/Alabay Merch/blue cap bck.png'
+import blueCap from '../Alabay Assets/Alabay Merch/cap mockup.png'
+import pinkBG from '../Alabay Assets/Alabay Merch/pink tee bck.png'
+import pinkTshirt from '../Alabay Assets/Alabay Merch/pink tshirt.png'
+import yellowBg from '../Alabay Assets/Alabay Merch/yellow tee bck.png'
+import yellowTshirt from '../Alabay Assets/Alabay Merch/yellow tshirt.png'
+
+
+
+// Games
+import game1 from '../Alabay Assets/Alabay Games/Abilities 2.png'
+import game2 from '../Alabay Assets/Alabay Games/Alabay Adventure - The Lost Heritage/Abilities.png'
+import game3 from '../Alabay Assets/Alabay Games/Alabay Adventure - The Lost Heritage/artifacts.png'
+import game4 from '../Alabay Assets/Alabay Games/Alabay Guard prev gradient.png'
+import game5 from '../Alabay Assets/Alabay Games/Alabay heritage prev gradient.png'
+import game6 from '../Alabay Assets/Alabay Games/alabay lost heritage prev 1.png'
+import game7 from '../Alabay Assets/Alabay Games/alabay guard prev 2 1.png'
+import game8 from '../Alabay Assets/Alabay Games/artifacts 2.png'
+import game9 from '../Alabay Assets/Alabay Games/image 3 alabay guard 2.png'
+
+export const GAMES = [game4, game5, game6, game7];
+export const GAMES_PREVIEW = [game1, game2, game3, game8, game9];
+
+export default ROAD_MAP;
+
+
+// Merchandise Store Launch
+
+export const IMAGES =  [img1, img2, img3, img4, img5, img6, img7];
+
+export const MERCHANDISE = [
+    img1 = {
+        desc : "Community Building and Initial Launch",
+        img: img6,
+        gradient: `bg-gradient-to-b from-[#DBC70C] to-[#FFF28000]`,
+        borderRadius: `rounded-tl-xl`,
+        linearGradient: `bg-gradient-to-t from-[#FFFFFF] to-[#FFFFFF00]`
+    },
+    img2 = {
+        desc : "Merchandise Store Launch",
+        img: img5,
+        gradient: `bg-gradient-to-b from-[#00D4D4] to-[#41FFFF00] `,
+        borderRadius: `rounded-b-xl`,
+        linearGradient: `bg-gradient-to-t from-[#FFFFFF] to-[#FFFFFF00]`
+    },
+    img3 = {
+        desc : "Community Events and contests",
+        img: img4,
+        gradient: `bg-gradient-to-b from-[#D427FF] to-[#D427FF00]`,
+        borderRadius: `rounded-b-xl`,
+        linearGradient: `bg-gradient-to-t from-[#FFFFFF] to-[#FFFFFF00]`
+    },
+    img4 = {
+        desc : "Expansion and New Features",
+        img: img7,
+        gradient: `bg-gradient-to-b from-[#8F3A3C] to-[#8F3A3C00]`,
+        borderRadius: `rounded-tr-xl`,
+        linearGradient: `bg-gradient-to-t from-[#FFFFFF] to-[#FFFFFF00]`
+    }
+]
+
+
+// let MER1, MER2, MER3, MER4;
+export const MERCHANDISES = [
+    {
+        bg: blackBG,
+        front: blackHoodies
+    },
+    {
+        bg: blueBG,
+        front: blueCap
+    },
+    {
+        bg: pinkBG,
+        front: pinkTshirt
+    },
+    {
+        bg: yellowBg,
+        front: yellowTshirt
+    }
+]
